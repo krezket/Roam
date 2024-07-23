d897136783948f2445de923f03bcc26a5ac75c80
Anthony <anthonyguerrero62@outlook.com>
2024-07-23 10:51:12 -0700

    added gradient animation, and button

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,39 @@
-import { Text, View, StyleSheet, Image } from "react-native";
-import { LinearGradient } from 'expo-linear-gradient';
+import { Text, View, StyleSheet, Image, useWindowDimensions, TouchableOpacity } from "react-native";
+import { Canvas, LinearGradient, Rect, vec } from "@shopify/react-native-skia";
+import { useDerivedValue, useSharedValue, withTiming } from "react-native-reanimated";
+import { StatusBar } from "expo-status-bar";
+import { getRandomColor } from "@/scripts/getRandomColor";
 
 export default function index() {
+  const { width, height } = useWindowDimensions();
+
+  const leftColor = useSharedValue('red');
+  const rightColor = useSharedValue('blue');
+
+  const colors = useDerivedValue(() => {
+    return [leftColor.value, rightColor.value];
+  }, []);
+
   return (
-    <LinearGradient
-      style={styles.container}
-      // colors={['gray', 'red', 'gray']}
-      colors={['#192f6a', '#3b5998', '#192f6a']}
-      >
-      <Text>Welcome to</Text>
-      <Image source={require("../assets/images/ROAM.png")} />
-    </LinearGradient>
+    <>
+      <StatusBar />
+      <Canvas style={{ flex: 1 }}>
+        <Rect x={0} y={0} width={width} height={height}>
+          <LinearGradient
+            start={vec(0, 0)}
+            end={vec(width, height)}
+            colors={colors}
+          />
+        </Rect>
+      </Canvas>
+      <TouchableOpacity
+        style={styles.container}
+        onPress={() => {
+          leftColor.value = withTiming(getRandomColor());
+          rightColor.value = withTiming(getRandomColor());
+        }}>
+      </TouchableOpacity>    
+    </>
   );
 };
 
---
+++ scripts/getRandomColor.ts
@@ -0,0 +1,6 @@
+export const getRandomColor = () => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`;
+};

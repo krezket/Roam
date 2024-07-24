9783884f4adf3dd3248cd382e5e99c09465ce9c2
Anthony Guerrero <krezket@Anthonys-Mac-Studio.local>
2024-07-23 22:13:11 -0700

    added new pngs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,20 @@
 import { Stack } from "expo-router";
+import { StyleSheet, View } from "react-native";
 
 export default function RootLayout() {
   return (
+      <View style={styles.container}>
+
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="+not-found" />
     </Stack>
+      </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
--- app/index.tsx
+++ app/index.tsx
@@ -1,30 +1,51 @@
-import React, { useEffect } from "react";
-import { Text, View, StyleSheet, Image, useWindowDimensions, TouchableOpacity } from "react-native";
+import React, { useRef, useEffect } from "react";
+import { PropsWithChildren } from "react";
+import { Animated, Text, View, StyleSheet, Image, useWindowDimensions, TouchableOpacity } from "react-native";
+import { ViewStyle } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { Canvas, LinearGradient, Rect, vec } from "@shopify/react-native-skia";
 import { useDerivedValue, useSharedValue, withTiming, withSequence, SharedValue } from "react-native-reanimated";
 import { StatusBar } from "expo-status-bar";
 import { getRandomColor } from "@/scripts/getRandomColor";
 
+type FadeInViewProps = PropsWithChildren<{style: ViewStyle}>;
+
+const FadeInView: React.FC<FadeInViewProps> = props => {
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 3000,
+      useNativeDriver: true,
+    }).start();
+  }, [fadeAnim]);
+
+  return (
+    <Animated.View // Special animatable View
+      style={{
+        ...props.style,
+        opacity: fadeAnim, // Bind opacity to animated value
+      }}>
+      {props.children}
+    </Animated.View>
+  );
+};
+
 export default function index() {
   const { width, height } = useWindowDimensions();
-
+  
   const leftColor = useSharedValue('red');
-  // const middleColor = useSharedValue('red');
   const rightColor = useSharedValue('black');
-
+  
   const colors = useDerivedValue(() => {
-    return [
-      leftColor.value, 
-      // middleColor.value, 
-      rightColor.value
-    ];
+    return [leftColor.value, rightColor.value];
   }, []);
-
+  
   useEffect(() => {
     const createContinuousTransition = (colorSharedValue: SharedValue<string>) => {
-      const colorSequence = Array.from({ length: 100 }, getRandomColor);
+      const colorSequence = Array.from({ length: 150 }, getRandomColor);
       const colorAnimations = colorSequence.map(color =>
-        withTiming(color, { duration: 3000 })
+        withTiming(color, { duration: 4000 })
       );
       colorSharedValue.value = withSequence(...colorAnimations);
     };
@@ -34,9 +55,9 @@ export default function index() {
 
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <StatusBar />
-      <Canvas style={StyleSheet.absoluteFill}>
+      <Canvas style={StyleSheet.absoluteFillObject}>
         <Rect x={0} y={0} width={width} height={height}>
           <LinearGradient
             start={vec(0, 0)}
@@ -45,11 +66,11 @@ export default function index() {
             />
         </Rect>
       </Canvas>
-      <View style={styles.textContainer}>
+      <FadeInView style={styles.textContainer}>
         <Text style={styles.text}>Welcome to</Text>
         <Image source={require('../assets/images/ROAM.png')} />
-      </View>   
-    </View>
+      </FadeInView>   
+    </SafeAreaView>
   );
 };
 

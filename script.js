f75802826db3afadd074615cc6820ec1776165b3
Anthony Guerrero <anthonyguerrero62@gmail.com>
2024-08-08 12:29:38 -0700

    saved

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-console.log("loaded")
-const canvas = document.getElementById("canvas")
-const ctx = canvas.getContext("2d")
+console.log("loaded");
+const canvas = document.getElementById("canvas");
+const rect = document.getElementById("rect");
+const ctx = canvas.getContext("2d");
 
 let x = 0;
 let y = 0;
@@ -9,10 +10,10 @@ let vx = 0;
 let vy = 0;
 
 function update() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     x += vx;
     y += vy;
-    ctx.fillRect(x, y, 50, 50)
-    requestAnimationFrame(update)
-}
-update()
+    ctx.fillRect(x, y, 10, 10);
+    requestAnimationFrame(update);
+};
+update();
